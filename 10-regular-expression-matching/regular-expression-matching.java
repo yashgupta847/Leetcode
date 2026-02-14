0f418143ce2060d2d14a83593c1f93ddc28e797d
Yash <yashgupta5046@gmail.com>
2026-02-14 10:29:01 +0530

    Time: 1 ms (99.71%) | Memory: 43.8 MB (37.18%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,25 +1,29 @@
 class Solution {
-    public boolean f(String s, String p, int i, int j) {
+    public boolean f(String s, String p, int i, int j, Boolean[][] dp) {
         if (i < 0 && j < 0)
             return true;
         if (j < 0)
             return false;
+
         if (i < 0) {
             //for (int l = 0; l <= j; l++) {
             if (p.charAt(j) == '*')
-                return f(s, p, i, j - 2);
+                return f(s, p, i, j - 2, dp);
             //}
             return false;
         }
-        if(p.charAt(j) == '.' || s.charAt(i) == p.charAt(j)) return f(s , p , i-1 , j-1);
+        if (dp[i][j] != null)
+            return dp[i][j];
+        if (p.charAt(j) == '.' || s.charAt(i) == p.charAt(j))
+            return dp[i][j] = f(s, p, i - 1, j - 1, dp);
         if (p.charAt(j) == '*') {
-            boolean skip = f(s, p, i, j - 2);
+            boolean skip = f(s, p, i, j - 2, dp);
             boolean take = false;
 
             if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i))
-                take = f(s, p, i - 1, j);
+                take = f(s, p, i - 1, j, dp);
 
-            return skip || take;
+            return dp[i][j] = skip || take;
         }
 
         return false;
@@ -27,7 +31,7 @@ class Solution {
     }
 
     public boolean isMatch(String s, String p) {
-       // boolean
-        return f(s, p, s.length() - 1, p.length() - 1);
+        Boolean dp[][] = new Boolean[s.length()][p.length()];
+        return f(s, p, s.length() - 1, p.length() - 1, dp);
     }
 }
