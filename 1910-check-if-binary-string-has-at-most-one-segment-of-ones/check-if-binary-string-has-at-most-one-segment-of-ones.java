5e6602ade4860ec8a6deb6b30167b9af7bb1d1f7
Yash <yashgupta5046@gmail.com>
2026-03-06 10:20:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (82.83%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,32 +1,32 @@
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1')
+                count++;
+        }
+        //for (int i = 0; i < s.length(); i++) {
+        int j = 0;
+        while (j < s.length() && s.charAt(j) == '1') {
+            j++;
+            count--;
+        }
+        if (count == 0)
+            return true;
+        else
+            return false;
+        // }
+      //  return true;
+    }
+}
+
 // class Solution {
 //     public boolean checkOnesSegment(String s) {
-//         int count = 0;
-//         for (int i = 0; i < s.length(); i++) {
-//             if (s.charAt(i) == '1')
-//                 count++;
-//         }
-//         for (int i = 0; i < s.length(); i++) {
-//             int j = i;
-//             while (j < s.length() && s.charAt(j) == '1') {
-//                 j++;
-//                 count--;
-//             }
-//             if (count == 0)
-//                 return true;
-//             else
+//         for(int i = 1; i < s.length(); i++){
+//             if(s.charAt(i) == '1' && s.charAt(i-1) == '0'){
 //                 return false;
+//             }
 //         }
 //         return true;
 //     }
 // }
-
-class Solution {
-    public boolean checkOnesSegment(String s) {
-        for(int i = 1; i < s.length(); i++){
-            if(s.charAt(i) == '1' && s.charAt(i-1) == '0'){
-                return false;
-            }
-        }
-        return true;
-    }
-}
