66e3abbaf5bffa8cb233c9d66d8eb36c5c467304
Yash <yashgupta5046@gmail.com>
2026-03-14 11:32:55 +0530

    Time: 18 ms (28.12%) | Memory: 47.1 MB (24.43%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,84 +1,84 @@
-class Solution {
-
-    public StringBuilder[] f(StringBuilder s, int[] i, StringBuilder[] arr, char prev , int n) {
-        
-        if (s.length() == n) {
-            arr[i[0]] = new StringBuilder(s);
-            i[0]++;
-            return arr;
-        }
-        if (prev != 'a') {
-            s.append('a');
-            arr = f(s, i, arr, 'a' , n);
-            s.deleteCharAt(s.length() - 1);
-        }  if (prev != 'b') {
-            s.append('b');
-            arr = f(s, i, arr, 'b' , n);
-            s.deleteCharAt(s.length() - 1);
-        } if(prev != 'c') {
-            s.append('c');
-            arr = f(s, i, arr, 'c' , n);
-            s.deleteCharAt(s.length() - 1);
-        }
-        return arr;
-    }
-
-    public String getHappyString(int n, int k) {
-        StringBuilder arr[] = new StringBuilder[3 * (int) Math.pow(2, n - 1)];
-        if (k > arr.length)
-            return "";
-        StringBuilder s = new StringBuilder();
-        
-        int[] i = new int[1];
-        arr = f( s , i , arr , '\0' ,n);
-        Arrays.sort(arr);
-        return arr[k-1].toString();
-    }
-}
-
 // class Solution {
 
-//     public int f(StringBuilder s, int i, StringBuilder[] arr, char prev, int n) {
-
+//     public StringBuilder[] f(StringBuilder s, int[] i, StringBuilder[] arr, char prev , int n) {
+        
 //         if (s.length() == n) {
-//             arr[i] = new StringBuilder(s);
-//             return i + 1;
+//             arr[i[0]] = new StringBuilder(s);
+//             i[0]++;
+//             return arr;
 //         }
-
 //         if (prev != 'a') {
 //             s.append('a');
-//             i = f(s, i, arr, 'a', n);
+//             arr = f(s, i, arr, 'a' , n);
 //             s.deleteCharAt(s.length() - 1);
-//         }
-
-//         if (prev != 'b') {
+//         }  if (prev != 'b') {
 //             s.append('b');
-//             i = f(s, i, arr, 'b', n);
+//             arr = f(s, i, arr, 'b' , n);
 //             s.deleteCharAt(s.length() - 1);
-//         }
-
-//         if (prev != 'c') {
+//         } if(prev != 'c') {
 //             s.append('c');
-//             i = f(s, i, arr, 'c', n);
+//             arr = f(s, i, arr, 'c' , n);
 //             s.deleteCharAt(s.length() - 1);
 //         }
-
-//         return i;
+//         return arr;
 //     }
 
 //     public String getHappyString(int n, int k) {
-
 //         StringBuilder arr[] = new StringBuilder[3 * (int) Math.pow(2, n - 1)];
-
 //         if (k > arr.length)
 //             return "";
-
 //         StringBuilder s = new StringBuilder();
-
-//         f(s, 0, arr, '\0', n);
-
+        
+//         int[] i = new int[1];
+//         arr = f( s , i , arr , '\0' ,n);
 //         Arrays.sort(arr);
-
-//         return arr[k - 1].toString();
+//         return arr[k-1].toString();
 //     }
 // }
+
+class Solution {
+
+    public int f(StringBuilder s, int i, StringBuilder[] arr, char prev, int n) {
+
+        if (s.length() == n) {
+            arr[i] = new StringBuilder(s);
+            return i + 1;
+        }
+
+        if (prev != 'a') {
+            s.append('a');
+            i = f(s, i, arr, 'a', n);
+            s.deleteCharAt(s.length() - 1);
+        }
+
+        if (prev != 'b') {
+            s.append('b');
+            i = f(s, i, arr, 'b', n);
+            s.deleteCharAt(s.length() - 1);
+        }
+
+        if (prev != 'c') {
+            s.append('c');
+            i = f(s, i, arr, 'c', n);
+            s.deleteCharAt(s.length() - 1);
+        }
+
+        return i;
+    }
+
+    public String getHappyString(int n, int k) {
+
+        StringBuilder arr[] = new StringBuilder[3 * (int) Math.pow(2, n - 1)];
+
+        if (k > arr.length)
+            return "";
+
+        StringBuilder s = new StringBuilder();
+
+        f(s, 0, arr, '\0', n);
+
+        Arrays.sort(arr);
+
+        return arr[k - 1].toString();
+    }
+}
