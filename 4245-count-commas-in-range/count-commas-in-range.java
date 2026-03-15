bebc9c587392300df8f54a88a3ebeada7705410e
Yash <yashgupta5046@gmail.com>
2026-03-15 10:13:13 +0530

    Time: 1 ms (87.49%) | Memory: 42.6 MB (73.73%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -5,14 +5,6 @@ class Solution {
         // if (s.length() <= 3)
         //     return 0;
         // // return s.length() / 4;
-        int cnt = 0;
-        for (int i = 1000; i <= n; i++) {
-            String s = Integer.toString(i);
-            if (s.length() % 3 == 0) {
-                cnt += s.length() / 3 - 1;
-            } else
-                cnt += s.length() / 3;
-        }
-        return cnt;
+        return Math.max(0, n - 999);
     }
 }
