09ea9198715f60647e3352aed32a75da6f6af76c
Yash <yashgupta5046@gmail.com>
2026-03-18 00:26:32 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (31.39%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -2,7 +2,7 @@ class Solution {
     public double f(double x, long n) {
         if (n == 0)
             return 1;
-        double half = myPow(x, n / 2);
+        double half = f(x, n / 2);
         if (n % 2 == 0) {
             return half * half;
         } else {
