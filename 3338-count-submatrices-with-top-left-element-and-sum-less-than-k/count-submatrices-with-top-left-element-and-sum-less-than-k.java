ee013f372e5f9f1d67e16b3d492054dcefc681d9
Yash <yashgupta5046@gmail.com>
2026-03-18 10:31:37 +0530

    Time: 3 ms (100.00%) | Memory: 161 MB (91.94%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -4,6 +4,7 @@ class Solution {
         if (grid[0][0] <= k)
             ans++;
         for (int i = 1; i < grid.length; i++) {
+
             grid[i][0] = grid[i - 1][0] + grid[i][0];
             if (grid[i][0] <= k)
                 ans++;
@@ -16,6 +17,8 @@ class Solution {
                 break;
         }
         for (int i = 1; i < grid.length; i++) {
+            if (grid[i][0] > k)
+                break;
             for (int j = 1; j < grid[0].length; j++) {
                 grid[i][j] += grid[i - 1][j] + grid[i][j - 1] - grid[i - 1][j - 1];
                 if (grid[i][j] <= k)
