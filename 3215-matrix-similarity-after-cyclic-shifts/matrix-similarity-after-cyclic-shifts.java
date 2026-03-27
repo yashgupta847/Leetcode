21d77fe20b9c214f05ca62a049043f7bf85f187f
Yash <yashgupta5046@gmail.com>
2026-03-27 09:22:38 +0530

    Time: 1 ms (99.47%) | Memory: 46.9 MB (64.00%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,88 @@
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        if (k > m)
+            k = k % m;
+        if (k == 0)
+            return true;
+        int[][] mat2 = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                mat2[i][j] = mat[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int l = 0;
+            int r = m - 1;
+            if (i % 2 == 0) {
+                l = 0;
+                r = k - 1;
+                int temp;
+                while (l <= r) {
+                    temp = mat2[i][l];
+                    mat2[i][l] = mat2[i][r];
+                    mat2[i][r] = temp;
+                    l++;
+                    r--;
+                }
+                l = k;
+                r = m - 1;
+                while (l <= r) {
+                    temp = mat2[i][l];
+                    mat2[i][l] = mat2[i][r];
+                    mat2[i][r] = temp;
+                    l++;
+                    r--;
+                }
+                l = 0;
+                r = m - 1;
+                while (l <= r) {
+                    temp = mat2[i][l];
+                    mat2[i][l] = mat2[i][r];
+                    mat2[i][r] = temp;
+                    l++;
+                    r--;
+                }
+            } else {
+                l = 0;
+                r = m - k - 1;
+                int temp;
+                while (l <= r) {
+                    temp = mat2[i][l];
+                    mat2[i][l] = mat2[i][r];
+                    mat2[i][r] = temp;
+                    l++;
+                    r--;
+                }
+                l = m - k;
+                r = m - 1;
+                while (l <= r) {
+                    temp = mat2[i][l];
+                    mat2[i][l] = mat2[i][r];
+                    mat2[i][r] = temp;
+                    l++;
+                    r--;
+                }
+                l = 0;
+                r = m - 1;
+                while (l <= r) {
+                    temp = mat2[i][l];
+                    mat2[i][l] = mat2[i][r];
+                    mat2[i][r] = temp;
+                    l++;
+                    r--;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] != mat2[i][j])
+                    return false;
+            }
+        }
+        return true;
+
+    }
+}
