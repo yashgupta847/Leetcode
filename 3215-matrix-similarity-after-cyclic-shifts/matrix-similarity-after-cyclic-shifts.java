0b92d82497e8976d2fbf9ba8f3c6d90dbeb3ed7f
Yash <yashgupta5046@gmail.com>
2026-03-27 09:26:25 +0530

    Time: 1 ms (99.47%) | Memory: 46.8 MB (91.20%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -1,4 +1,15 @@
 class Solution {
+    public void reverse(int[][] mat2, int l, int r , int i) {
+        int temp;
+        while (l <= r) {
+            temp = mat2[i][l];
+            mat2[i][l] = mat2[i][r];
+            mat2[i][r] = temp;
+            l++;
+            r--;
+        }
+    }
+
     public boolean areSimilar(int[][] mat, int k) {
         int n = mat.length;
         int m = mat[0].length;
@@ -19,61 +30,24 @@ class Solution {
             if (i % 2 == 0) {
                 l = 0;
                 r = k - 1;
-                int temp;
-                while (l <= r) {
-                    temp = mat2[i][l];
-                    mat2[i][l] = mat2[i][r];
-                    mat2[i][r] = temp;
-                    l++;
-                    r--;
-                }
+                reverse(mat2, l, r , i);
                 l = k;
                 r = m - 1;
-                while (l <= r) {
-                    temp = mat2[i][l];
-                    mat2[i][l] = mat2[i][r];
-                    mat2[i][r] = temp;
-                    l++;
-                    r--;
-                }
+                reverse(mat2, l, r , i);
                 l = 0;
                 r = m - 1;
-                while (l <= r) {
-                    temp = mat2[i][l];
-                    mat2[i][l] = mat2[i][r];
-                    mat2[i][r] = temp;
-                    l++;
-                    r--;
-                }
+                reverse(mat2, l, r , i);
             } else {
                 l = 0;
                 r = m - k - 1;
                 int temp;
-                while (l <= r) {
-                    temp = mat2[i][l];
-                    mat2[i][l] = mat2[i][r];
-                    mat2[i][r] = temp;
-                    l++;
-                    r--;
-                }
+                reverse(mat2, l, r , i);
                 l = m - k;
                 r = m - 1;
-                while (l <= r) {
-                    temp = mat2[i][l];
-                    mat2[i][l] = mat2[i][r];
-                    mat2[i][r] = temp;
-                    l++;
-                    r--;
-                }
+                reverse(mat2, l, r , i);
                 l = 0;
                 r = m - 1;
-                while (l <= r) {
-                    temp = mat2[i][l];
-                    mat2[i][l] = mat2[i][r];
-                    mat2[i][r] = temp;
-                    l++;
-                    r--;
-                }
+                reverse(mat2, l, r , i);
             }
         }
         for (int i = 0; i < n; i++) {
