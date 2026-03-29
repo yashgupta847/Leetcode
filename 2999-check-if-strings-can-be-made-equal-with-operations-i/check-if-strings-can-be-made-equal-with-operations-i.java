dfec4e60e3493f41b258753ae9333bccc191a017
Yash <yashgupta5046@gmail.com>
2026-03-29 10:05:18 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (11.22%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,26 +1,12 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        int cnt = 0;
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i))
-                cnt++;
-        }
-        if (cnt == 0)
-            return true;
-        if (cnt == 4) {
-            if (s1.charAt(0) == s2.charAt(2) && s1.charAt(1) == s2.charAt(3)) {
-                if (s1.charAt(2) == s2.charAt(0) && s1.charAt(3) == s2.charAt(1))
-                    return true;
-            }
-        }
-        if (cnt == 2) {
-            if (s1.charAt(0) == s2.charAt(2) && s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3) && s1.charAt(2) == s2.charAt(0))
-                return true;
-            if (s2.charAt(3) == s1.charAt(1) && s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2) && s1.charAt(3) == s2.charAt(1))
-                return true;
-        }
-        if (cnt % 2 != 0)
-            return false;
-        return false;
+        boolean evenMatch = 
+            (s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+            (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0));
+        boolean oddMatch = 
+            (s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+            (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1));
+
+        return evenMatch && oddMatch;
     }
 }
