1dac38d7e3ed9a92a4e3474b5d2f48f01856f4f3
Yash <yashgupta5046@gmail.com>
2026-04-02 10:24:35 +0530

    Time: 292 ms (5.62%) | Memory: 278 MB (20.22%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -1,42 +1,46 @@
 class Solution {
-    public int f(int[][] coins, int i, int j, int dilute , Integer[][][] dp) {
-        if(i >= coins.length || j >= coins[0].length) return Integer.MIN_VALUE;
+    public int f(int[][] coins, int i, int j, int dilute, Integer[][][] dp) {
+        if (i >= coins.length || j >= coins[0].length)
+            return Integer.MIN_VALUE;
+        if (dp[i][j][dilute] != null)
+            return dp[i][j][dilute];
+
         if (i == coins.length - 1 && j == coins[0].length - 1) {
             if (coins[i][j] >= 0)
-                return coins[i][j];
+                return dp[i][j][dilute] = coins[i][j];
             else {
                 if (dilute == 2)
-                    return coins[i][j];
-                return 0;
+                    return dp[i][j][dilute] = coins[i][j];
+                return dp[i][j][dilute] = 0;
             }
         }
-        if(dp[i][j][dilute] != null) return dp[i][j][dilute];
         int right = Integer.MIN_VALUE;
         if (coins[i][j] < 0 && dilute < 2) {
-            int skip = f(coins, i, j + 1, dilute + 1 , dp);
-            int next = f(coins, i, j + 1, dilute , dp);
+            int skip = f(coins, i, j + 1, dilute + 1, dp);
+            int next = f(coins, i, j + 1, dilute, dp);
             int take = (next == Integer.MIN_VALUE) ? Integer.MIN_VALUE : coins[i][j] + next;
             right = Math.max(skip, take);
         } else {
-            int next = f(coins, i, j + 1, dilute , dp);
-            if(next != Integer.MIN_VALUE)
+            int next = f(coins, i, j + 1, dilute, dp);
+            if (next != Integer.MIN_VALUE)
                 right = coins[i][j] + next;
         }
         int down = Integer.MIN_VALUE;
         if (coins[i][j] < 0 && dilute < 2) {
-            int skip = f(coins, i + 1, j, dilute + 1 , dp);
-            int next = f(coins, i + 1, j, dilute , dp);
+            int skip = f(coins, i + 1, j, dilute + 1, dp);
+            int next = f(coins, i + 1, j, dilute, dp);
             int take = (next == Integer.MIN_VALUE) ? Integer.MIN_VALUE : coins[i][j] + next;
             down = Math.max(skip, take);
         } else {
-            int next = f(coins, i + 1, j, dilute , dp);
-            if(next != Integer.MIN_VALUE)
+            int next = f(coins, i + 1, j, dilute, dp);
+            if (next != Integer.MIN_VALUE)
                 down = coins[i][j] + next;
         }
-        return dp[i][j][dilute] =  Math.max(down, right);
+        return dp[i][j][dilute] = Math.max(down, right);
     }
+
     public int maximumAmount(int[][] coins) {
         Integer[][][] dp = new Integer[coins.length][coins[0].length][3];
-        return f(coins, 0, 0, 0 , dp);
+        return f(coins, 0, 0, 0, dp);
     }
 }
