5bb91d0f04c3706a8e3371af7d7c2477d81e59ed
Yash <yashgupta5046@gmail.com>
2026-04-06 09:57:30 +0530

    Time: 24 ms (69.93%) | Memory: 50 MB (93.48%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.java
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -1,36 +1,68 @@
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
 
-        HashSet<List<Integer>> hs = new HashSet<>();
-        for (int[] o : obstacles) {
-            hs.add(Arrays.asList(o[0], o[1]));
-        }
         int x = 0, y = 0;
-        int dir = 0;
-        int[][] d = {{0,1},{1,0},{0,-1},{-1,0}};
         int max = 0;
-        for (int cmd : commands) {
-            if (cmd == -1) {
-                dir = (dir + 1) % 4;
-            } 
-            else if (cmd == -2) {
-                dir = (dir + 3) % 4;
-            } 
-            else {
 
-                for (int i = 0; i < cmd; i++) {
+        int up = 1, down = 0, left = 0, right = 0;
+        HashSet<List<Integer>> hs = new HashSet<>();
+        for (int i = 0; i < obstacles.length; i++) {
+            hs.add(Arrays.asList(obstacles[i][0], obstacles[i][1]));
+        }
 
-                    int nx = x + d[dir][0];
-                    int ny = y + d[dir][1];
+        for (int i = 0; i < commands.length; i++) {
+            int now = commands[i];
 
+            if (1 <= now && now <= 9) {
+
+                for (int step = 0; step < now; step++) {
+
+                    int nx = x, ny = y;
+
+                    if (up == 1)
+                        ny++;
+                    else if (down == 1)
+                        ny--;
+                    else if (left == 1)
+                        nx--;
+                    else
+                        nx++;
                     if (hs.contains(Arrays.asList(nx, ny))) {
                         break;
                     }
-
                     x = nx;
                     y = ny;
 
-                    max = Math.max(max, x*x + y*y);
+                    max = Math.max(max, x * x + y * y);
+                }
+            } else if (now == -1) {
+                if (up == 1) {
+                    up = 0;
+                    right = 1;
+                } else if (right == 1) {
+                    right = 0;
+                    down = 1;
+                } else if (down == 1) {
+                    down = 0;
+                    left = 1;
+                } else {
+                    left = 0;
+                    up = 1;
+                }
+
+            } else {
+                if (up == 1) {
+                    up = 0;
+                    left = 1;
+                } else if (left == 1) {
+                    left = 0;
+                    down = 1;
+                } else if (down == 1) {
+                    down = 0;
+                    right = 1;
+                } else {
+                    right = 0;
+                    up = 1;
                 }
             }
         }
