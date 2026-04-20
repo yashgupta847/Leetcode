6d738f428aa940ff8bb326fadeddf54908bfd708
Yash <yashgupta5046@gmail.com>
2026-04-20 10:02:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (45.54%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
@@ -1,19 +1,23 @@
 class Solution {
-    public int f(int[] colors, int i, int j, int[][] dp) {
+    // public int f(int[] colors, int i, int j, int[][] dp) {
 
-        if (i > j || i > colors.length - 1 || j < 0)
-            return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if (colors[i] == colors[j]) {
-            return dp[i][j] = Math.max(f(colors, i + 1, j, dp), f(colors, i, j - 1, dp));
-        }
-        return dp[i][j] = Math.abs(j - i);
-    }
+    //     if (i > j || i > colors.length - 1 || j < 0)
+    //         return 0;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     if (colors[i] == colors[j]) {
+    //         return dp[i][j] = Math.max(f(colors, i + 1, j, dp), f(colors, i, j - 1, dp));
+    //     }
+    //     return dp[i][j] = Math.abs(j - i);
+    // }
 
     public int maxDistance(int[] colors) {
-        int[][] dp = new int[colors.length][colors.length];
-        for (int[] dp1 : dp)
-            Arrays.fill(dp1, -1);
-        return f(colors, 0, colors.length - 1, dp);
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < colors.length; i++) {
+            if (colors[i] != colors[0])
+                ans = Math.max(ans, i - 0);
+            if (colors[i] != colors[colors.length - 1])
+                ans = Math.max(ans, colors.length - 1 - i);
+        }
+        return ans;
     }
 }
