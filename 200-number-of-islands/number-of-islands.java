8b70b4b0b407c8e3f1030ed8298c4e5647c454ba
Yash <yashgupta5046@gmail.com>
2026-04-21 19:50:16 +0530

    Time: 12 ms (5.03%) | Memory: 52 MB (77.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,40 +1,33 @@
 class Solution {
     public int numIslands(char[][] grid) {
         boolean[][] visit = new boolean[grid.length][grid[0].length];
-        Queue<int[]> q = new LinkedList<>();
-        // for (int i = 0; i < grid.length; i++) {
-        //     for(int j =0 ; j < grid[0].length ; j++){
-        //         if(grid[i][j] == '1'){
-        //            q.add(new int[]{i,j})
-        //         }
-        //     }
-        // }
+        Stack<int[]> q = new Stack<>();
         int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1' && !visit[i][j]) {
                     visit[i][j] = true;
-                    q.add(new int[] { i, j });
+                    q.push(new int[] { i, j });
                     count++;
                     while (!q.isEmpty()) {
-                        int[] a = q.poll();
+                        int[] a = q.pop();
                         int x = a[0];
                         int y = a[1];
                         visit[x][y] = true;
                         if (x + 1 <= grid.length - 1 && grid[x + 1][y] == '1' && !visit[x + 1][y]) {
-                            q.add(new int[] { x + 1, y });
+                            q.push(new int[] { x + 1, y });
                             visit[x + 1][y] = true;
                         }
                         if (y + 1 <= grid[0].length - 1 && grid[x][y + 1] == '1' && !visit[x][y + 1]) {
-                            q.add(new int[] { x, y + 1 });
+                            q.push(new int[] { x, y + 1 });
                             visit[x][1 + y] = true;
                         }
                         if (y - 1 >= 0 && grid[x][y - 1] == '1' && !visit[x][y - 1]) {
-                            q.add(new int[] { x, y - 1 });
+                            q.push(new int[] { x, y - 1 });
                             visit[x][y - 1] = true;
                         }
                         if (x - 1 >= 0 && grid[x - 1][y] == '1' && !visit[x - 1][y]) {
-                            q.add(new int[] { x - 1, y });
+                            q.push(new int[] { x - 1, y });
                             visit[x - 1][y] = true;
                         }
                     }
