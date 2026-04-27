4ca835220d6cf3d9f670f3d0bc6e3143c890e7c3
Yash <yashgupta5046@gmail.com>
2026-04-27 18:07:37 +0530

    Time: 9 ms (94.65%) | Memory: 47.7 MB (60.79%) - LeetSync

---
+++ 1508-longest-happy-prefix/longest-happy-prefix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int[] lps = new int[s.length()];
+        lps[0] = 0;
+        int len = 0;
+        int n = s.length();
+        for (int i = 1; i < n;) {
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return s.substring(0 , lps[s.length()-1]);
+    }
+}
