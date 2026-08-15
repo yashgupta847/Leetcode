75c9c0c8ce6aafdc891249006b7f8d988a556925
Yash <yashgupta5046@gmail.com>
2026-08-15 10:48:11 +0530

    Time: 7 ms (36.62%) | Memory: 45.3 MB (53.44%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,30 +1,27 @@
 class Solution {
+    public void f(List<List<Integer>> ans, int[] c, int t, int idx, ArrayList<Integer> demo) {
+        if (t == 0) {
+            ans.add(new ArrayList<>(demo));
+            return;
+        }
+        if (idx == c.length || t < 0)
+            return;
+
+        demo.add(c[idx]);
+        f(ans, c, t - c[idx], idx + 1, demo);
+        demo.remove(demo.size() - 1);
+        int i = idx;
+        while (i + 1 < c.length && c[idx] == c[i + 1]) {
+            i++;
+        }
+        f(ans, c, t, i + 1, demo);
+
+    }
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> ans = new ArrayList<>();
-        f(candidates, target, 0, ans, new ArrayList<>());
+        f(ans, candidates, target, 0, new ArrayList<>());
         return ans;
-    }
-
-    public void f(int[] n, int target, int idx, List<List<Integer>> ans, List<Integer> res) {
-
-        if (target == 0) {
-            ans.add(new ArrayList<>(res));
-            return;
-        }
-
-        if (idx == n.length || target < 0)
-            return;
-
-        res.add(n[idx]);
-        f(n, target - n[idx], idx + 1, ans, res);
-        res.remove(res.size() - 1);
-
-        int nextIdx = idx;
-        while (nextIdx + 1 < n.length && n[nextIdx] == n[nextIdx + 1]) {
-            nextIdx++;
-        }
-
-        f(n, target, nextIdx + 1, ans, res);
     }
 }
