e63aa51317bb86a066ade3a7b5462e340dfa79c0
Yash <yashgupta5046@gmail.com>
2026-08-31 23:57:40 +0530

    Time: 91 ms (25.72%) | Memory: 123.3 MB (5.32%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,29 +1,29 @@
 class Solution {
-    public boolean f(int t, int[] nums, int i, Boolean[][] dp) {
-        if (i == 0) {
-            if (nums[0] == t)
-                return true;
-            return false;
-        }
-        if (dp[i][t] != null)
-            return dp[i][t];
-        if (t == 0)
+    public boolean f(int[] nums, int target, int idx, Boolean[][] dp) {
+        if (idx == nums.length)
+            return target == 0;
+        if (target == 0)
             return true;
-        boolean notTake = f(t, nums, i - 1, dp);
-        boolean take = false;
-        if (nums[i] <= t) {
-            take = f(t - nums[i], nums, i - 1, dp);
-        }
-        return dp[i][t] = take || notTake;
+        if (target < 0)
+            return false;
+        if (dp[idx][target] != null)
+            return dp[idx][target];
+
+        boolean take = f(nums, target - nums[idx], idx + 1, dp);
+        boolean nottake = f(nums, target, idx + 1, dp);
+        return dp[idx][target] = take || nottake;
     }
 
     public boolean canPartition(int[] nums) {
-        int total = 0;
-        for (int i = 0; i < nums.length; i++)
-            total += nums[i];
-        if (total % 2 != 0)
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 != 0)
             return false;
-        Boolean[][] dp = new Boolean[nums.length][total / 2 + 1];
-        return f(total / 2, nums, nums.length - 1, dp);
+        Boolean[][] dp = new Boolean[nums.length][sum];
+        boolean[] used = new boolean[nums.length];
+        return f(nums, sum / 2, 0, dp);
     }
 }
