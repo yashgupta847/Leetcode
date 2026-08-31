9208f46b35cc6ce214e49139fe9eb3e7fac509a2
Yash <yashgupta5046@gmail.com>
2026-08-31 22:12:24 +0530

    Time: 7 ms (98.76%) | Memory: 45.8 MB (10.74%) - LeetSync

--- 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.java
+++ 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.java
@@ -1,17 +1,21 @@
 class Solution {
-    public int solve(int[][] c, int idx, int prev) {
+    public int solve(int[][] c, int idx, int prev, int[][] dp) {
         if (idx == c.length)
             return 0;
+        if (dp[idx][prev + 1] != -1)
+            return dp[idx][prev + 1];
         int take = 0;
         if (prev == -1 || (c[prev][0] <= c[idx][0] && c[prev][1] <= c[idx][1] && c[prev][2] <= c[idx][2])) {
-            take = c[idx][2] + solve(c, idx + 1, idx);
+            take = c[idx][2] + solve(c, idx + 1, idx, dp);
         }
-        int nottake = solve(c, idx + 1, prev);
-        return Math.max(take, nottake);
-
+        int nottake = solve(c, idx + 1, prev, dp);
+        return dp[idx][prev + 1] = Math.max(take, nottake);
     }
 
     public int maxHeight(int[][] c) {
+        int[][] dp = new int[c.length][c.length + 1];
+        for (int[] dp1 : dp)
+            Arrays.fill(dp1, -1);
         for (int i = 0; i < c.length; i++) {
             Arrays.sort(c[i]);
         }
@@ -22,6 +26,6 @@ class Solution {
                 return a[1] - b[1];
             return a[2] - b[2];
         });
-        return solve(c, 0, -1);
+        return solve(c, 0, -1, dp);
     }
 }
