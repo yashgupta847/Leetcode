ff72b611a546a51d72b100527663a76a0b1b4d30
Yash <yashgupta5046@gmail.com>
2026-09-06 17:32:37 +0530

    Time: 42 ms (78.41%) | Memory: 46.1 MB (86.77%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,10 +1,9 @@
 class Solution {
-    public boolean compare(String[] words, int i, int j) {
-        if (words[i].length() != 1 + words[j].length()) {
-            return false;
-        }
+    public boolean compare(int i, int j, String[] words) {
         String a = words[i];
         String b = words[j];
+        if (1 + a.length() != b.length())
+            return false;
         int l = 0;
         int r = 0;
         int diff = 0;
@@ -18,34 +17,27 @@ class Solution {
                 if (diff > 1)
                     return false;
 
-                l++;
+                r++;
             }
         }
         return true;
     }
 
     public int longestStrChain(String[] words) {
-        Arrays.sort(words, (a, b) -> b.length() - a.length());
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
         int[] dp = new int[words.length];
-        // for (int[] dp1 : dp)
-        Arrays.fill(dp, -1);
+        Arrays.fill(dp, 1);
         int maxi = 1;
         for (int i = 0; i < words.length; i++) {
-            maxi = Math.max(maxi, f(i, words, dp));
-        }
-        return maxi;
-    }
-
-    public int f(int idx, String[] words, int[] dp) {
-        if (dp[idx] != -1)
-            return dp[idx];
-
-        int ans = 1;
-        for (int j = idx + 1; j < words.length; j++) {
-            if (compare(words, idx, j)) {
-                ans = Math.max(ans, 1 + f(j, words, dp));
+            for (int j = i + 1; j < words.length; j++) {
+                if (compare(i, j, words) && 1 + dp[i] > dp[j]) {
+                    dp[j] = 1 + dp[i];
+                }
             }
         }
-        return dp[idx] = ans;
+        for (int i = 0; i < words.length; i++) {
+            maxi = Math.max(dp[i], maxi);
+        }
+        return maxi;
     }
 }
