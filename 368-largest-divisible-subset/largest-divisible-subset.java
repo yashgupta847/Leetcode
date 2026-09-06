f13bffbb7fe5a42ac258129516b1f2dc5a6e1f73
Yash <yashgupta5046@gmail.com>
2026-09-06 17:09:13 +0530

    Time: 17 ms (63.09%) | Memory: 45.3 MB (43.34%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -1,30 +1,30 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        Arrays.sort(nums);
-        int max = 1;
-        int maxIdx = 0;
-        int dp[] = new int[nums.length];
-        int hash[] = new int[nums.length];
-        Arrays.fill(dp, 1);
         List<Integer> ans = new ArrayList<>();
-        for (int i = 1; i < nums.length; i++) {
+        int[] dp = new int[nums.length];
+        int[] hash = new int[nums.length];
+        Arrays.sort(nums);
+        int max = 0;
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
             hash[i] = i;
             for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
+                if (nums[i] % nums[j] == 0 && 1 + dp[j] > dp[i]) {
+                    dp[i] = 1 + dp[j];
                     hash[i] = j;
                 }
             }
-            if (dp[i] > max) {
-                max = dp[i];
-                maxIdx = i;
+            if (dp[i] > dp[max]) {
+                max = i;
             }
         }
-        while (hash[maxIdx] != maxIdx) {
-            ans.add(nums[maxIdx]);
-            maxIdx = hash[maxIdx];
+        int idx = max;
+
+        ans.add(nums[idx]);
+        while (hash[idx] != idx) {
+            ans.add(nums[hash[idx]]);
+            idx = hash[idx];
         }
-        ans.add(nums[maxIdx]);
         return ans;
     }
 }
