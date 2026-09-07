49f26562c44cd48688965864d9d3e7ea883afbab
Yash <yashgupta5046@gmail.com>
2026-09-07 17:13:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (89.35%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,21 +1,23 @@
 class Solution {
-    public int f(int[] nums, int idx, int canTake, int last , int[][] dp) {
-        if (idx == last)
+
+    public int f(int[] nums, int idx, int end, int[] dp) {
+        if (idx >= end + 1)
             return 0;
-        if(dp[idx][canTake] != -1) return dp[idx][canTake];
-        if (canTake == 1) {
-            return dp[idx][canTake] = Math.max(f(nums, idx + 1, 0, last , dp) + nums[idx], f(nums, idx + 1, 1, last , dp));
-        } else {
-            return dp[idx][canTake] =  f(nums, idx + 1, 1, last , dp);
-        }
+        if (dp[idx] != -1)
+            return dp[idx];
+
+        int take = f(nums, idx + 2, end, dp) + nums[idx];
+        int nottake = f(nums, idx + 1, end, dp);
+        return dp[idx] = Math.max(take, nottake);
     }
 
     public int rob(int[] nums) {
-        int[][] dp = new int[nums.length][2];
-        int[][] dp2 = new int[nums.length][2];
-        for(int[] dp1 : dp) Arrays.fill(dp1 , -1);
-        for(int[] dp1 : dp2) Arrays.fill(dp1 , -1);
-        if(nums.length == 1) return nums[0];
-        return Math.max(f(nums, 0, 1, nums.length - 1 , dp), f(nums, 1, 1, nums.length , dp2));
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        int one = f(nums, 0, nums.length - 2, dp);
+        Arrays.fill(dp, -1);
+        int two = f(nums, 1, nums.length - 1, dp);
+        if(nums.length==1) return nums[0];
+        return Math.max(one, two);
     }
 }
