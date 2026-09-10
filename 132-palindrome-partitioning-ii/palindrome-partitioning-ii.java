4450599f87ddf0f42f6a2a6ec7c4b57aeb507f8f
Yash <yashgupta5046@gmail.com>
2026-09-11 01:53:55 +0530

    Time: 2278 ms (33.16%) | Memory: 43.1 MB (67.64%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,31 +1,38 @@
 class Solution {
-    int[] dp;
-    public int minCut(String s) {
-        int n = s.length();
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-        return f(0, s) - 1;
-    }
-    int f(int i, String s) {
-        int n = s.length();
-        if (i == n) return 0;
-        if (dp[i] != -1) return dp[i];  
-        int minCuts = Integer.MAX_VALUE;
-        for (int j = i; j < n; j++) {
-            if (isPalindrome(s, i, j)) {
-                int cuts = 1 + f(j + 1, s);
-                minCuts = Math.min(minCuts, cuts);
-            }
+    public boolean isPalindrome(String s , int i , int j) {
+        int l = i;
+        int r = j;
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+            r--;
+            l++;
         }
-        return dp[i] = minCuts;
+
+        return true;
     }
 
-    boolean isPalindrome(String s, int l, int r) {
-        while (l < r) {
-            if (s.charAt(l) != s.charAt(r)) return false;
-            l++;
-            r--;
+    public int f(String s, int i, int[] dp) {
+        if (i == s.length())
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        // String temp = "";
+        int min_cost = Integer.MAX_VALUE;
+        for (int k = i; k < s.length(); k++) {
+            // temp += s.charAt(k);
+            if (isPalindrome(s , i , k)) {
+                int cost = 1 + f(s, k + 1 , dp);
+                min_cost = Math.min(min_cost, cost);
+            }
         }
-        return true;
+        return dp[i] = min_cost;
+
+    }
+
+    public int minCut(String s) {
+        int[] dp = new int[s.length()];
+        Arrays.fill(dp, -1);
+        return f(s, 0, dp) - 1;
     }
 }
