debfac0a36caa557ac16f6926651667e60981798
Yash <yashgupta5046@gmail.com>
2026-09-12 10:41:42 +0530

    Time: 2 ms (10.62%) | Memory: 47.1 MB (10.89%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int[][] ans = new int[image.length][image[0].length];
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[0].length; j++) {
+                ans[i][j] = image[i][j];
+            }
+        }
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] { sr, sc, image[sr][sc] });
+        ans[sr][sc] = color;
+        boolean[][] visited = new boolean[image.length][image[0].length];
+        int[][] dirs = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+        while (!q.isEmpty()) {
+            int[] a = q.remove();
+            int x = a[0];
+            int y = a[1];
+            int nowColor = a[2];
+
+            visited[x][y] = true;
+            for (int[] dir : dirs) {
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+                if (nx >= 0 && nx < image.length && ny >= 0 && ny < image[0].length && !visited[nx][ny]
+                        && image[nx][ny] == nowColor) {
+                    visited[nx][ny] = true;
+                    q.add(new int[] { nx, ny, image[nx][ny] });
+                    ans[nx][ny] = color;
+                }
+            }
+        }
+        return ans;
+    }
+}
