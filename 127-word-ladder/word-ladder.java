99ee6f4bf841a29dd3982f54fcb454053581e82c
Yash <yashgupta5046@gmail.com>
2026-09-14 10:37:05 +0530

    Time: 128 ms (18.18%) | Memory: 49.3 MB (23.43%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -6,23 +6,21 @@ class Solution {
         for (int i = 0; i < wordList.size(); i++) {
             hs.add(wordList.get(i));
         }
-
         while (!q.isEmpty()) {
-            String a[] = q.poll();
-            String element = a[0];
-            int level = Integer.parseInt(a[1]);
-            if (element.equals(endWord))
-                return level;
-            for (int i = 0; i < element.length(); i++) {
-                StringBuilder s = new StringBuilder(element);
-                char ch = s.charAt(i);
-                for (char j = 'a'; j <= 'z'; j++) {
-                    if (j != ch) {
-                        s.setCharAt(i, j);
-                        if (hs.contains(s.toString())) {
-                            q.add(new String[] { s.toString(), String.valueOf(level + 1) });
-                            hs.remove(s.toString());
+            String[] arr = q.remove();
+            String a = arr[0];
+            int now = Integer.parseInt(arr[1]);
+            if (a.equals(endWord))
+                return now;
 
+            for (int i = 0; i < a.length(); i++) {
+                for (char j = 'a'; j <= 'z'; j++) {
+                    StringBuilder st = new StringBuilder(a);
+                    if (j != st.charAt(i)) {
+                        st.setCharAt(i, j);
+                        if (hs.contains(st.toString())) {
+                            q.add(new String[] { st.toString(), String.valueOf(now + 1) });
+                            hs.remove(st.toString());
                         }
                     }
                 }
