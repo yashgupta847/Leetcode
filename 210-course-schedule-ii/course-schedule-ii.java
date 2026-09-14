2b9b109fa22893126d9bf3c882e43c4e5256c635
Yash <yashgupta5046@gmail.com>
2026-09-15 00:54:34 +0530

    Time: 7 ms (29.30%) | Memory: 46.9 MB (39.08%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -4,44 +4,39 @@ class Solution {
         for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
-        for (int edge[] : prerequisites) {
-            int u = edge[0];
-            int v = edge[1];
-            adj.get(u).add(v);
+        for (int i = 0; i < prerequisites.length; i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj.get(v).add(u);
         }
-        Queue<Integer> q = new LinkedList<>();
         int[] inD = new int[numCourses];
         for (int i = 0; i < adj.size(); i++) {
-            ArrayList<Integer> a = adj.get(i);
-            for (int e : a) {
-                inD[e]++;
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                inD[adj.get(i).get(j)]++;
             }
         }
-        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < inD.length; i++) {
-            if (inD[i] == 0) {
+            if (inD[i] == 0)
                 q.add(i);
-                ans.add(i);
-            }
         }
+        ArrayList<Integer> ans1 = new ArrayList<>();
         while (!q.isEmpty()) {
-            int a = q.poll();
-            for (int neighb : adj.get(a)) {
-                inD[neighb]--;
-                if (inD[neighb] == 0) {
-                    q.add(neighb);
-                    ans.add(neighb);
-                }
+            int a = q.remove();
+            ans1.add(a);
+            for (int n : adj.get(a)) {
+                inD[n]--;
+                if (inD[n] == 0)
+                    q.add(n);
             }
         }
-        if (ans.size() != numCourses) {
+        if (ans1.size() != numCourses)
             return new int[] {};
+        int[] ans = new int[numCourses];
+        // int i = 0;
+        for(int i = 0 ; i < ans1.size() ; i++){
+            ans[i] = ans1.get(i);
         }
-        int[] a = new int[numCourses];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = ans.remove(ans.size() - 1);
-        }
-        return a;
-
+        return ans;
     }
 }
