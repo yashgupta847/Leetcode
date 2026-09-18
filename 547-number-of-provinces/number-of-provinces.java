e422b27a3e713da25d3077c5abc58202fe12c9b7
Yash <yashgupta5046@gmail.com>
2026-09-18 18:30:07 +0530

    Time: 2 ms (40.94%) | Memory: 46.9 MB (96.75%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,37 +1,54 @@
 class Solution {
+    class DSU {
+        int[] parent;
+        int[] rank;
+
+        public DSU(int n) {
+            parent = new int[n + 1];
+            rank = new int[n + 1];
+            for (int i = 0; i <= n; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+            }
+        }
+
+        int findParent(int node) {
+            if (parent[node] == node) {
+                return node;
+            }
+            int pU = findParent(parent[node]);
+            return parent[node] = pU;
+        }
+
+        void union(int u, int v) {
+            int parentU = findParent(u);
+            int parentV = findParent(v);
+            if (parentU == parentV)
+                return;
+            if (rank[parentU] > rank[parentV]) {
+                parent[parentV] = parentU;
+            } else if (rank[parentV] > rank[parentU]) {
+                parent[parentU] = parentV;
+            } else {
+                rank[parentU]++;
+                parent[parentV] = parentU;
+            }
+        }
+    }
+
     public int findCircleNum(int[][] isConnected) {
-
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        DSU ds = new DSU(isConnected.length );
         for (int i = 0; i < isConnected.length; i++) {
-            adj.add(new ArrayList<>());
-        }
-        for (int i = 0; i < isConnected.length; i++) {
-            for (int j = 0; j < isConnected[i].length; j++) {
+            for (int j = 0; j < isConnected[0].length; j++) {
                 if (isConnected[i][j] == 1) {
-                    adj.get(i).add(j);
+                    ds.union(i+1, j+1);
                 }
             }
         }
-        boolean[] visited = new boolean[isConnected.length];
-        Stack<Integer> st = new Stack<>();
-        int connected = 0;
-
-        for (int i = 0; i < isConnected.length; i++) {
-            if (!visited[i]) {
-                connected += 1;
-                st.add(i);
-                visited[i] = true;
-                while (!st.isEmpty()) {
-                    int a = st.pop();
-                    for (int neighb : adj.get(a)) {
-                        if (!visited[neighb]) {
-                            st.add(neighb);
-                            visited[neighb] = true;
-                        }
-                    }
-                }
-            }
+        int ans = 0;
+        for(int i = 1 ; i < isConnected.length+1 ;i++){
+            if(ds.parent[i] == i) ans++;
         }
-        return connected;
+        return ans;
     }
 }
